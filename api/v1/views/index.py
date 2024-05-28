ef9b1e3798aa3ff3eedfa8497bd8890efc230a0c
PKgothatso <ksp331@outlook.com>
2024-05-28 13:02:36 -0700

    Errors

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-'''Contains the index view for the API.'''
-from flask import jsonify
+"""Contains the index view for the API."""
 
+from flask import jsonify
 from api.v1.views import app_views
 from models import storage
 from models.amenity import Amenity
@@ -14,15 +14,13 @@ from models.user import User
 
 @app_views.route('/status')
 def get_status():
-    '''Gets the status of the API.
-    '''
+    """Gets the status of the API."""
     return jsonify(status='OK')
 
 
 @app_views.route('/stats')
 def get_stats():
-    '''Gets the number of objects for each type.
-    '''
+    """Gets the number of objects for each type."""
     objects = {
             'amenities': Amenity,
             'cities': City,
