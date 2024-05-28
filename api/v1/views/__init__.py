fe6ae7c372f052cf5e7a5a985ad656def65ad0ae
PKgothatso <ksp331@outlook.com>
2024-05-28 14:23:52 -0700

    Errors

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
-""" Blueprint for API """
+'''Contains the blueprint for the API.'''
 from flask import Blueprint
-from api.v1.views.index import
-from api.v1.views.states import
-from api.v1.views.places import
-from api.v1.views.places_reviews import
-from api.v1.views.cities import handle_cities, get_cities
-from api.v1.views.amenities import AmenityView, handle_amenities,
-from api.v1.views.users import UsersView
-from api.v1.views.places_amenities import Places_AmentiesView,
-handle_places_amenities, get_place_amenities
+
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+'''The blueprint for the AirBnB clone API.'''
+
+
+from api.v1.views.amenities import *
+from api.v1.views.cities import *
+from api.v1.views.index import *
+from api.v1.views.places_amenities import *
+from api.v1.views.places import *
+from api.v1.views.places_reviews import *
+from api.v1.views.states import *
+from api.v1.views.users import *
