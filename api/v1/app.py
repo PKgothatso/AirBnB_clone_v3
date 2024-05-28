e5c32720b508937b966d5117f684cf29b7e722ac
PKgothatso <ksp331@outlook.com>
2024-05-28 12:21:22 -0700

    Updates

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,10 +2,11 @@
 """Contains a Flask web application API.
 """
 import os
-from flask import Flask, Blueprint, jsonify
+from flask import Flask, jsonify
 from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
+from flasgger import Swagger
 
 
 app = Flask(__name__)
@@ -15,6 +16,26 @@ app_port = int(os.getenv('HBNB_API_PORT', '5000'))
 app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
 CORS(app, resources={'/*': {'origins': app_host}})
+
+app.config['SWAGGER'] = {
+    "swagger_version": "2.0",
+    "title": "Flasgger",
+    "headers": [
+        ('Access-Control-Allow-Methods', "GET, POST, PUT, DELETE, OPTIONS),
+        ('Access-Control-Allow-Origin', '*"),
+        ('Access-Control-Allow-Credentials', "true"),
+    ],
+    "specs": [
+        {
+            "title": "HBNB API",
+            "description": "AirBnB RESTful API",
+            "version": "1",
+            "endpoint": 'v1_views',
+            "route": '/v1/views',
+        }
+    ]
+}
+swagger = Swagger(app)
 
 
 @app.teardown_appcontext
@@ -41,4 +62,4 @@ def error_400(error):
 if __name__ == '__main__':
     app_host = os.getenv('HBNB_API_HOST', '0.0.0.0')
     app_port = int(os.getenv('HBNB_API_PORT', '5000'))
-    app.run(host=app_host, port=app_port)
+    app.run(host=app_host, port=app_port, threaded=True)
