936006b60239f7b1f5e05c88ce440382012fa4c2
PKgothatso <ksp331@outlook.com>
2024-05-28 09:41:02 -0700

    Error fixes

--- api/v1/app.py
+++ api/v1/app.py
@@ -20,7 +20,6 @@ CORS(app, resources={'/*': {'origins': app_host}})
 @app.teardown_appcontext
 def teardown_flask(exception):
     '''The Flask app/request context end event listener.'''
-    # print(exception)
     storage.close()
 
 
@@ -43,4 +42,3 @@ if __name__ == '__main__':
     app_host = os.getenv('HBNB_API_HOST', '0.0.0.0')
     app_port = int(os.getenv('HBNB_API_PORT', '5000'))
     app.run(host=app_host, port=app_port)
-    threaded = True
