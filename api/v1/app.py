0ce17acdb10325f29d71d1b81dba65c3291d85b3
PKgothatso <ksp331@outlook.com>
2024-05-28 12:48:24 -0700

    Fixing errors

--- api/v1/app.py
+++ api/v1/app.py
@@ -11,8 +11,7 @@ from flasgger.utils import swag_from
 
 app = Flask(__name__)
 """The Flask web application instance."""
-app_host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-app_port = int(os.getenv('HBNB_API_PORT', '5000'))
+app.config['JSONIFY_BOLDPRINT_REGULAR'] = True
 app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
 CORS(app, resources={'/*': {'origins': app_host}})
