0d1b2908ba65b94a33eb357f1f114c4e748cb3d6
PKgothatso <ksp331@outlook.com>
2024-05-28 14:01:09 -0700

    fixing errors

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
-"""Contains the blueprint for the API."""
+""" Blueprint for API """
 from flask import Blueprint
+from api.v1.views.index import
+from api.v1.views.states import
+from api.v1.views.places import
+from api.v1.views.places_reviews import
+from api.v1.views.cities import handle_cities, get_cities
+from api.v1.views.amenities import AmenityView, handle_amenities,
+from api.v1.views.users import UsersView
+from api.v1.views.places_amenities import Places_AmentiesView,
+handle_places_amenities, get_place_amenities
+
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-from.index import *
-from.states import *
-from.cities import *
-from.amenities import *
-from.users import *
-from.places import *
-from.places_reviews import *
